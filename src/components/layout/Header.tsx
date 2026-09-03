efa3e238be56f5da079f9727558f8bf1f3016d3e
Dimas <dimasars79@gmail.com>
2026-09-03 09:48:04 +0700

    design: maximize UKPBJ logo size in navbar

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -208,7 +208,7 @@ export function Header() {
             <Link href="/" className="flex-shrink-0 flex items-center space-x-2 sm:space-x-4">
               <Image src="/logo-kemnaker.png" alt="Logo Kementerian Ketenagakerjaan" width={64} height={64} className="h-10 sm:h-12 md:h-16 w-auto object-contain drop-shadow-sm" />
               <div className="h-8 sm:h-10 md:h-14 border-l border-slate-300"></div>
-              <Image src="/logo-ukpbj-kemnaker-new.png" alt="Logo UKPBJ" width={300} height={120} className="h-16 sm:h-20 md:h-28 w-auto object-contain drop-shadow-sm scale-110 origin-left" />
+              <Image src="/logo-ukpbj-kemnaker-new.png" alt="Logo UKPBJ" width={400} height={160} className="h-20 sm:h-24 md:h-36 w-auto object-contain drop-shadow-sm scale-125 origin-left translate-x-2" />
             </Link>
           </div>
 
